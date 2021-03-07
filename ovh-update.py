dfc868de4cc79f41c695d04bdc518ddd2840aed7
mathieugery <mathieu.gery@epitech.eu>
2021-03-07 22:47:32 +0100

    Add sopme commets

--- ovh-update.py
+++ ovh-update.py
@@ -15,7 +15,6 @@ client = ovh.Client(
     application_secret='XXXXXXXX', # Application Secret
     consumer_key="XXXXXXXX", # Consumer Key
 )
-
 domain_name = "XXXXXXXX" # Enter your OVH domain name here
 
 def check_if_file_exist():
@@ -44,11 +43,14 @@ def get_dns_records(previous_ip):
     return (records_info)
 
 def update_dns(previous_ip, new_ip):
+    #This is print if you want more logs you can add this here
+    #Print previous and new ip
     print("previous", previous_ip)
     print("newip", new_ip)
-    # Pretty print
+    # Pretty print for all records that match the ip
     print (json.dumps(get_dns_records(previous_ip), indent=4))
     print(dns_records[1]["id"])
+
     for record in dns_records:
         result = client.put("/domain/zone/" + domain_name + "/record/" + str(record["id"]),
         subDomain=record["subDomain"], # Resource record subdomain (type: string)
@@ -57,14 +59,15 @@ def update_dns(previous_ip, new_ip):
         )
 
 def check_ip_and_update():
-    process = subprocess.Popen(["dig", "TXT", "+short", "o-o.myaddr.l.google.com", "@ns1.google.com"], stdout=subprocess.PIPE)
+    process = subprocess.Popen(["dig", "TXT", "+short", "o-o.myaddr.l.google.com", "@ns1.google.com"], stdout=subprocess.PIPE) #Run bash command to get the current pub ip
+    #Pars the output
     output, error = process.communicate()
     output = output[1:-2]
     ext_ip = output.decode('ascii')
-    check_if_file_exist()
+    check_if_file_exist() #Check if the file neede to store the old ip is present. If not create it
     f = open("current_ip.txt", "r+")
     previous_ip = f.read()
-    if (previous_ip != ext_ip):
+    if (previous_ip != ext_ip): #Read the old ip and compare to the current ip
         f.close()
         f = open("current_ip.txt", "w")
         f.write(ext_ip)
